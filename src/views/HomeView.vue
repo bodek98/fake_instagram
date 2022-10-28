72d2188ff18ca2fdf7002191da61bd2e52b25db2
bodek98 <bodzio.entire@gmail.com>
2022-10-28 22:09:25 +0200

    New files with routing and json-server database

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        {{ post.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from "axios";
+
+export default {
+  name: "HomeView",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  async created() {
+    try {
+      const res = await axios.get(`http://localhost:3000/posts`);
+      this.posts = res.data;
+    } catch (e) {
+      console.error(e);
+    }
+  },
+};
+</script>

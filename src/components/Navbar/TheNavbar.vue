5a31436db3f016fef5ce117d420e08fa6fe787d0
bodek98 <bodzio.entire@gmail.com>
2022-11-05 17:45:31 +0100

    Updated scss to correct BEM

--- src/components/Navbar/TheNavbar.vue
+++ src/components/Navbar/TheNavbar.vue
@@ -1,51 +1,51 @@
 <template>
-  <nav v-if="!isNavHidden" class="navbar--top navbar--white">
-    <router-link class="navbar__p--instagram" to="/">fakeInstagram</router-link>
+  <nav v-if="!isNavHidden" class="navbar navbar--top">
+    <router-link class="navbar__logo" to="/">fakeInstagram</router-link>
     <input class="navbar__search" type="search" placeholder="Search" />
     <a class="navbar__heart">
       <font-awesome-icon icon="fa-regular fa-heart" />
     </a>
   </nav>
-  <nav v-if="!isNavHidden" class="navbar--bottom navbar--white">
-    <router-link class="navbar__a" to="/">
+  <nav v-if="!isNavHidden" class="navbar navbar--bottom">
+    <router-link class="navbar__link" to="/">
       <font-awesome-icon icon="fa-solid fa-house" />
     </router-link>
-    <router-link class="navbar__a" to="/explore">
+    <router-link class="navbar__link" to="/explore">
       <font-awesome-icon icon="fa-solid fa-compass" />
     </router-link>
-    <a class="navbar__a">
+    <a class="navbar__link">
       <font-awesome-icon icon="fa-regular fa-square-plus" />
     </a>
-    <router-link class="navbar__a" to="/message">
+    <router-link class="navbar__link" to="/message">
       <font-awesome-icon icon="fa-solid fa-comment" />
     </router-link>
-    <router-link class="navbar__a" to="/profile">
+    <router-link class="navbar__link" to="/profile">
       <font-awesome-icon icon="fa-solid fa-user" />
     </router-link>
   </nav>
-  <nav v-else class="navbar navbar--left navbar--white">
-    <button class="navbar__button navbar__a navbar__a--instagram">
+  <nav v-else class="navbar navbar--left">
+    <button class="navbar__button navbar__link navbar__link--instagram">
       <font-awesome-icon icon="fa-brands fa-instagram" />
     </button>
-    <router-link class="navbar__a" to="/">
+    <router-link class="navbar__link" to="/">
       <font-awesome-icon icon="fa-solid fa-house" />
     </router-link>
-    <button class="navbar__button navbar__a">
+    <button class="navbar__button navbar__link">
       <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
     </button>
-    <router-link class="navbar__a" to="/explore">
+    <router-link class="navbar__link" to="/explore">
       <font-awesome-icon icon="fa-solid fa-compass" />
     </router-link>
-    <router-link class="navbar__a" to="/message">
+    <router-link class="navbar__link" to="/message">
       <font-awesome-icon icon="fa-solid fa-comment" />
     </router-link>
-    <button class="navbar__button navbar__a">
+    <button class="navbar__button navbar__link">
       <font-awesome-icon icon="fa-regular fa-heart" />
     </button>
-    <button class="navbar__button navbar__a">
+    <button class="navbar__button navbar__link">
       <font-awesome-icon icon="fa-regular fa-square-plus" />
     </button>
-    <router-link class="navbar__a" to="/profile">
+    <router-link class="navbar__link" to="/profile">
       <font-awesome-icon icon="fa-solid fa-user" />
     </router-link>
   </nav>

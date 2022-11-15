05023d417d33f869f2777a9730ed39443ed436d8
bodek98 <bodzio.entire@gmail.com>
2022-11-15 23:54:27 +0100

    Refactored fetching API to vuex store (scrolling not working well)

--- src/components/Posts/PostSection.vue
+++ src/components/Posts/PostSection.vue
@@ -21,7 +21,6 @@
 
 <script>
 import PostSingle from "./SinglePost/PostSingle.vue";
-import axios from "axios";
 import { ref, onMounted } from "vue";
 import { useStore } from "vuex";
 
@@ -31,50 +30,26 @@ export default {
   },
   setup() {
     const store = useStore();
-    const posts = ref([]);
+    const posts = ref(store.state.posts);
     const newPosts = ref(store.state.newPosts);
     let page = ref(1);
-
-    const getNewPosts = () => {};
-    const getPosts = onMounted(async () => {
-      try {
-        await axios
-          .get(`http://localhost:3000/posts?_page=${page.value}&_limit=5`)
-          .then((res) => {
-            posts.value = res.data;
-          });
-      } catch (error) {
-        console.error(error);
-      }
-    });
-    const scrollPosts = onMounted(async () => {
-      await axios
-        .get(`http://localhost:3000/posts?_page=${page.value}&_limit=5`)
-        .then((res) => {
-          page.value++;
-          posts.value = [...posts.value, ...res.data];
-        });
-    });
     const handleScroll = (isVisible) => {
       if (!isVisible) return;
       if (page.value >= 21) return;
       if (window.scrollY + window.innerHeight >= document.body.scrollHeight) {
-        scrollPosts();
+        store.dispatch("fetchScrolledPosts");
+        page.value++;
       }
     };
-    const infiniteScroll = onMounted(() => {
-      window.addEventListener("scroll", handleScroll);
-    });
-
     onMounted(() => {
-      getPosts();
-      infiniteScroll();
+      store.dispatch("fetchPosts");
+      // store.dispatch("fetchScrolledPosts")
+      window.addEventListener("scroll", handleScroll);
     });
 
     return {
       posts,
       newPosts,
-      getNewPosts,
     };
   },
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,22 @@
 import { createStore } from "vuex";
+import { ref } from "vue";
 import createPersistedState from "vuex-persistedstate";
+import axios from "axios";
 
+let page = ref(1);
 const store = createStore({
   state: {
     newPosts: [],
+    posts: [],
+    page: 1,
   },
   mutations: {
+    GET_POSTS: (state, posts) => {
+      state.posts = posts;
+    },
+    GET_SCROLLEDPOSTS: (state, posts) => {
+      state.posts = [...state.posts, ...posts];
+    },
     ADD_POST: (state, newPost) => {
       state.newPosts.unshift(newPost);
     },
@@ -16,7 +27,29 @@ const store = createStore({
       state.newPosts.splice(objWithIdIndex, 1);
     },
   },
+  actions: {
+    async fetchPosts({ commit }) {
+      try {
+        const data = await axios.get(
+          `http://localhost:3000/posts?_page=1&_limit=5`
+        );
+        commit("GET_POSTS", data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async fetchScrolledPosts({ commit }) {
+      try {
+        const data = await axios.get(
+          `http://localhost:3000/posts?_page=${page.value}&_limit=5`
+        );
+        page.value++;
+        commit("GET_SCROLLEDPOSTS", data.data);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
   plugins: [createPersistedState()],
 });
-
 export default store;
